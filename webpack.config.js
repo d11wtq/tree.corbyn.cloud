177cbbf70117eb2a6abac9b02d474f0f056109d6
d11wtq <chris@w3style.co.uk>
2021-03-31 12:09:04 +1100

    Get CSS Working. Downgrade to Webpack 4.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   target: 'web',
   mode: process.env.NODE_ENV || 'development',
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
     new HtmlWebpackPlugin({
       filename: path.resolve(
         __dirname,
         'index.html',
       ),
     }),
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
@@ -19,15 +25,21 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
   entry: {
-    app: './src/bundle.js',
+    app: [
+      './src/bundle.js',
+      './src/bundle.css',
+    ],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     publicPath: 'dist',
-    clean: true,
   },
 };
