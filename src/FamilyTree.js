e1ecf117ea1616768b9f438146a48b25a86c3e3c
d11wtq <chris@w3style.co.uk>
2021-03-31 13:58:57 +1100

    Auto-scroll tree to midpoint on load.

--- src/FamilyTree.js
+++ src/FamilyTree.js
@@ -1,8 +1,24 @@
-const React = require('react');
+const React = {useRef, useEffect} = require('react');
 
 const FamilyTree = ({children}) => {
+  const ref = useRef(null);
+
+  // Scroll the tree to the center for initial display.
+  useEffect(() => {
+    const element = ref.current;
+
+    if (element && window && typeof window.getComputedStyle == 'function') {
+      const computedStyle = getComputedStyle(element);
+
+      element.scrollTo(
+        (element.scrollWidth / 2) - (parseInt(computedStyle.width) / 2),
+        0,
+      );
+    }
+  });
+
   return (
-    <div className="family-tree">
+    <div className="family-tree" ref={ref}>
       {children}
     </div>
   );
